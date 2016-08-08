92fa8d7ff119eab5aaf80eed12df7c68e5b2a9b6
jennvance <vancejenniferm@gmail.com>
2016-08-08 11:35:00 -0600

    refactored slice

--- utils/main.js
+++ utils/main.js
@@ -3,39 +3,41 @@ var fs = require('fs');
 (function(messageFolder){
     var messageInfo = [];
  	var messages = fs.readdirSync(messageFolder);
-    for(var i in messages){
+    for (var i in messages){
         var fileName = messages[i];
         var contents = fs.readFileSync(messageFolder+'/'+ fileName, 'utf-8');
         var rows = contents.split('\n');
   		var singleMessage = {name:fileName};
-        for(var j in rows){
-        	if(rows[j].slice(0,4) === "From" ) {
-                if(rows[j].slice(5).charAt(0) === " "){
-                    singleMessage.from = rows[j].slice(6);
+        for (var j in rows){
+            var  arr = rows[j].split(":");
+            if (arr[0] === "From"){
+                if(arr[1].charAt(0) === " "){
+                    singleMessage.from = arr[1].slice(1);
+                    console.log(arr[1].slice(1))
                 } else {
-                    singleMessage.from = rows[j].slice(5);
-                }
-        	}
-        	if(rows[j].slice(0,4) === "Date" ) {
-                if(rows[j].slice(5).charAt(0) === " "){
-                    singleMessage.date = rows[j].slice(6);
-                } else {
-                    singleMessage.date = rows[j].slice(5);
-                }
-        	}
-        	if(rows[j].slice(0,7) === "Subject") {
-                if(rows[j].slice(8).charAt(0) === " "){
-                    singleMessage.subject = rows[j].slice(9);
-                } else {
-                    singleMessage.subject = rows[j].slice(8);
-                }
+                    singleMessage.from = arr[1];
+                };
             }
-        }
-        if(singleMessage.from !== undefined && singleMessage.date !== undefined && singleMessage.subject !== undefined) {
+            if (arr[0] === "Date"){
+                if(arr[1].charAt(0) === " "){
+                    singleMessage.date = arr[1].slice(1);
+                } else {
+                    singleMessage.date = arr[1];
+                };
+            }
+            if (arr[0] === "Subject"){
+                if(arr[1].charAt(0) === " "){
+                    singleMessage.subject = arr[1].slice(1);
+                } else {
+                    singleMessage.subject = arr[1];
+                };
+            }
+        };
+        if (singleMessage.from !== undefined && singleMessage.date !== undefined && singleMessage.subject !== undefined) {
     		messageInfo.push(singleMessage);
-    	}
+    	};
         fs.writeFileSync("results.json", JSON.stringify(messageInfo));
-    }
+    };
     console.log(messageInfo)
     return messageInfo;
 })('dataset');
